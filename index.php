0863297705ebcf9a24b03af29de851843c011f78
hlsmelo <hlsmelo@github.com>
2021-06-25 00:02:15 -0300

    Add index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SW Persons</title>
+</head>
+<body>
+    <script> document.body.SWData = { };</script>
+    <?php require_once './back/model.php'; ?>
+    <?php require_once './back/controller.php'; ?>
+    <?php require_once './back/viewer.php'; ?>
+    <?php require_once './back/router.php'; ?>
+</body>
+</html>
+
